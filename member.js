18bd3ca3a56b21e727f8abc808c3a4f28a501217
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-11-03 11:09:07 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,5 @@
-export interface SkillsMember {
-    name: string;
-    age: number;
-    email: string;
-}
 
-export function createSkillsMember(name: string, age: number, email: string): SkillsMember {
+
+export function createSkillsMember(name, age, email) {
     return { name, age, email };
 }
