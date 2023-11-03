a63327c7e7fc0dd07eb62937140a832d336f2df4
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-11-03 11:30:27 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,5 +1,16 @@
 
+export const skills = ['HTML', 'CSS', 'JS'];
 
-export function createSkillsMember(name, age, email) {
-    return { name, age, email };
+export const age = 13;
+
+export default class Member {
+    constructor(name) {
+        this.name = name;
+    }
 }
+export function createSkillsMember(name, age, email) {
+    return { name, age, email }; 
+}   
+
+
+createSkillsMember('pit', 13, 'pit@pit.com')
